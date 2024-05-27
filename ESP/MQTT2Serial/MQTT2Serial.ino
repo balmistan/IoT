b85efcf17c618f5a686a43ad74d0a576a9d2a378
Daniele <sangiovanni.daniele@gmail.com>
2024-05-27 16:33:10 +0200

    second serial port yet to be tested

---
+++ ESP/MQTT2Serial/MQTT2Serial.ino
@@ -0,0 +1,148 @@
+
+#include <ESP8266WiFi.h>
+#include <SoftwareSerial.h>
+#include <PubSubClient.h>
+#include "wifi_mqtt_connect.h"
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+SoftwareSerial mySerial = SoftwareSerial(D3, D4);  //GPIO0, GPIO2
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE (50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+void setup_serial_ports() {
+  pinMode(D3, INPUT);
+  pinMode(D4, OUTPUT);
+  mySerial.begin(9600);
+  Serial.begin(115200);
+}
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+/*
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+*/
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+    mySerial.print((char)payload[i]);
+  }
+  Serial.println();
+  mySerial.println();
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str(), mqtt_user, mqtt_password)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("toNodeRed", "connected!");
+      // ... and resubscribe
+      client.subscribe("fromNodeRed");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);  // Initialize the BUILTIN_LED pin as an output
+  setup_serial_ports();
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+}
+
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  boolean receved_msg = false;
+  char buffer[20];
+  int i=0;
+
+  
+    while(mySerial.available() > 0) {
+      receved_msg=true;
+    // read the incoming byte:
+    buffer[i++] = mySerial.read();
+    }
+    buffer[i]='\0';
+  if (receved_msg) {
+    receved_msg = false;
+    snprintf(msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("toNodeRed", msg);
+  }
+}
+
+/**
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("toNodeRed", msg);
+  }
+}
+**/
---
+++ ESP/MQTT2Serial/wifi_mqtt_connect.h
@@ -0,0 +1,14 @@
+#ifndef WIFI_MQTT_CONNECT_H
+#define WIFI_MQTT_CONNECT_H
+
+// Replace these with your WiFi network settings
+
+const char* ssid = "KabelBox-F7C0";
+const char* password = "98132235162191702181";
+const char* mqtt_server = "192.168.0.22";
+const char mqtt_user[] = "admin";
+const char mqtt_password[] = "dalisan19780";
+int mqtt_port = 1883;
+
+#endif
+
